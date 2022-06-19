89618408bcd387bb9c0f2312a882292ff6c128b1
German <Makeev_German@mail.ru@example.com>
2022-06-19 12:18:03 +0300

    the third digit

---
+++ dz/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+if (number<100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+// if(number>99 $$ number<1000 )
+// {
+//     Console.WriteLine(number%10);
+// }
+// else{
+//     string number1=number.ToString();
+//     Console.WriteLine(number1[2]);
+// }
+else
+{
+    while(number>999)
+    {number=number/10;
+    }
+    Console.WriteLine(number%10);
+}
